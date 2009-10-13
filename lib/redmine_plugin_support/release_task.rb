6a4017a431821208b848d6255e5500849c0b17f0
Eric Davis <edavis@littlestreamsoftware.com>
2009-10-13 13:30:17 -0700

    Allow the zip and tarball tasks to work without cleaning the tree.
    Since both tasks are using what's stored in git, cleaning the working copy is
    no longer needed.

--- lib/redmine_plugin_support/release_task.rb
+++ lib/redmine_plugin_support/release_task.rb
@@ -1,14 +1,17 @@
 module RedminePluginSupport
   class ReleaseTask < GeneralTask
     def define
+      desc "Create packages"
+      task :release => ['release:zip', 'release:tarball']
+      
       namespace :release do
         desc "Create a zip archive"
-        task :zip => [:clean] do
+        task :zip do
           sh "git archive --format=zip --prefix=#{Base.instance.project_name}/ HEAD > #{Base.instance.project_name}.zip"
         end
 
         desc "Create a tarball archive"
-        task :tarball => [:clean] do
+        task :tarball do
           sh "git archive --format=tar --prefix=#{Base.instance.project_name }/ HEAD | gzip > #{Base.instance.project_name}.tar.gz"
         end
       end
