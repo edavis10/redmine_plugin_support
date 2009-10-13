262a679d1f90ce678f86d165e47b86acb629970a
Eric Davis <edavis@littlestreamsoftware.com>
2009-10-13 15:18:27 -0700

    Some progress on rspec code stats

--- lib/redmine_plugin_support/stats_task.rb
+++ lib/redmine_plugin_support/stats_task.rb
@@ -17,23 +17,25 @@ module RedminePluginSupport
     end
     
     def define
+      # TODO: Need to get rspec stats to work
+      
       namespace :spec do
         task :statsetup do
           require 'code_statistics'
-          ::STATS_DIRECTORIES << %w(Model\ specs spec/models) if File.exist?('spec/models')
-          ::STATS_DIRECTORIES << %w(View\ specs spec/views) if File.exist?('spec/views')
-          ::STATS_DIRECTORIES << %w(Controller\ specs spec/controllers) if File.exist?('spec/controllers')
-          ::STATS_DIRECTORIES << %w(Helper\ specs spec/helpers) if File.exist?('spec/helpers')
-          ::STATS_DIRECTORIES << %w(Library\ specs spec/lib) if File.exist?('spec/lib')
-          ::STATS_DIRECTORIES << %w(Routing\ specs spec/routing) if File.exist?('spec/routing')
-          ::STATS_DIRECTORIES << %w(Integration\ specs spec/integration) if File.exist?('spec/integration')
-          ::CodeStatistics::TEST_TYPES << "Model specs" if File.exist?('spec/models')
-          ::CodeStatistics::TEST_TYPES << "View specs" if File.exist?('spec/views')
-          ::CodeStatistics::TEST_TYPES << "Controller specs" if File.exist?('spec/controllers')
-          ::CodeStatistics::TEST_TYPES << "Helper specs" if File.exist?('spec/helpers')
-          ::CodeStatistics::TEST_TYPES << "Library specs" if File.exist?('spec/lib')
-          ::CodeStatistics::TEST_TYPES << "Routing specs" if File.exist?('spec/routing')
-          ::CodeStatistics::TEST_TYPES << "Integration specs" if File.exist?('spec/integration')
+          STATS_DIRECTORIES << %w(Model\ specs spec/models) if File.exist?("#{RedmineHelper.plugin_root}/spec/models")
+          STATS_DIRECTORIES << %w(View\ specs spec/views) if File.exist?("#{RedmineHelper.plugin_root}/spec/views")
+          STATS_DIRECTORIES << %w(Controller\ specs spec/controllers) if File.exist?("#{RedmineHelper.plugin_root}/spec/controllers")
+          STATS_DIRECTORIES << %w(Helper\ specs spec/helpers) if File.exist?("#{RedmineHelper.plugin_root}/spec/helpers")
+          STATS_DIRECTORIES << %w(Library\ specs spec/lib) if File.exist?("#{RedmineHelper.plugin_root}/spec/lib")
+          STATS_DIRECTORIES << %w(Routing\ specs spec/routing) if File.exist?("#{RedmineHelper.plugin_root}/spec/routing")
+          STATS_DIRECTORIES << %w(Integration\ specs spec/integration) if File.exist?("#{RedmineHelper.plugin_root}/spec/integration")
+          ::CodeStatistics::TEST_TYPES << "Model specs" if File.exist?("#{RedmineHelper.plugin_root}/spec/models")
+          ::CodeStatistics::TEST_TYPES << "View specs" if File.exist?("#{RedmineHelper.plugin_root}/spec/views")
+          ::CodeStatistics::TEST_TYPES << "Controller specs" if File.exist?("#{RedmineHelper.plugin_root}/spec/controllers")
+          ::CodeStatistics::TEST_TYPES << "Helper specs" if File.exist?("#{RedmineHelper.plugin_root}/spec/helpers")
+          ::CodeStatistics::TEST_TYPES << "Library specs" if File.exist?("#{RedmineHelper.plugin_root}/spec/lib")
+          ::CodeStatistics::TEST_TYPES << "Routing specs" if File.exist?("#{RedmineHelper.plugin_root}/spec/routing")
+          ::CodeStatistics::TEST_TYPES << "Integration specs" if File.exist?("#{RedmineHelper.plugin_root}/spec/integration")
         end
       end
 
