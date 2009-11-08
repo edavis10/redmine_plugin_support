51e6f8e8ae9c3da68930f387e5a755dad8efaca8
Eric Davis <edavis@littlestreamsoftware.com>
2009-11-07 18:21:19 -0800

    Require preparing the test database before running tests

--- lib/redmine_plugin_support/test_unit_task.rb
+++ lib/redmine_plugin_support/test_unit_task.rb
@@ -19,28 +19,28 @@ module RedminePluginSupport
       end
 
       namespace :test do
-        Rake::TestTask.new(:units => :environment) do |t|
+        Rake::TestTask.new(:units => [:environment, 'db:test:prepare']) do |t|
           t.libs << "test"
           t.pattern = 'test/unit/**/*_test.rb'
           t.verbose = true
         end
         Rake::Task['test:units'].comment = "Run the unit tests in test/unit"
 
-        Rake::TestTask.new(:functionals => :environment) do |t|
+        Rake::TestTask.new(:functionals => [:environment, 'db:test:prepare']) do |t|
           t.libs << "test"
           t.pattern = 'test/functional/**/*_test.rb'
           t.verbose = true
         end
         Rake::Task['test:functionals'].comment = "Run the functional tests in test/functional"
 
-        Rake::TestTask.new(:integration => :environment) do |t|
+        Rake::TestTask.new(:integration => [:environment, 'db:test:prepare']) do |t|
           t.libs << "test"
           t.pattern = 'test/integration/**/*_test.rb'
           t.verbose = true
         end
         Rake::Task['test:integration'].comment = "Run the integration tests in test/integration"
 
-        Rake::TestTask.new(:benchmark => :environment) do |t|
+        Rake::TestTask.new(:benchmark => [:environment, 'db:test:prepare']) do |t|
           t.libs << 'test'
           t.pattern = 'test/performance/**/*_test.rb'
           t.verbose = true
@@ -48,7 +48,7 @@ module RedminePluginSupport
         end
         Rake::Task['test:benchmark'].comment = 'Benchmark the performance tests'
 
-        Rake::TestTask.new(:profile => :environment) do |t|
+        Rake::TestTask.new(:profile => [:environment, 'db:test:prepare']) do |t|
           t.libs << 'test'
           t.pattern = 'test/performance/**/*_test.rb'
           t.verbose = true
